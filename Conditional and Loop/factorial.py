2e1c08a91eefdad33e7276f8105976387d84eb56
Prakhar Gupta <prakhargupta3510@gmail.com>
2025-03-23 23:22:43 +0530

    Update factorial.py

--- Conditional and Loop/factorial.py
+++ Conditional and Loop/factorial.py
@@ -1,6 +1,7 @@
 number = int(input("Enter the value of n = "))
 fact = 1
+temp_number = number
 while number:
     fact=fact*number
     number-=1
-print("Factorial = ",fact)
+print("Factorial of ",temp_number," = ",fact) 
