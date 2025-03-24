c3f830bbe38f95c72d34a576bb48391f59ef457c
Prakhar Gupta <prakhargupta3510@gmail.com>
2025-03-24 22:59:45 +0530

    Update reverse.py

--- Conditional and Loop/reverse.py
+++ Conditional and Loop/reverse.py
@@ -1,7 +1,7 @@
 num = int(input("Enter the number = "))
 rev = 0
 while num>0:
-    r = num%10
-    rev = rev*10+r
+    d = num%10
+    rev = rev*10+d
     num = num//10
 print("Reversed number = ",rev)
